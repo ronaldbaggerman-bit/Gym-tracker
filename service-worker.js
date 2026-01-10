ff8560f274a25e8bbd63a56bb6347a95c6399ee7
ronaldbaggerman-bit <ronald.baggerman@gmail.com>
2026-01-10 09:48:19 +0100

    Bump service worker cache version to v2026-01-10-01 to force PWA update

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
  * Enables offline functionality and caching
  */
 
-const CACHE_NAME = 'gym-tracker-v2026-01-09-13';
+const CACHE_NAME = 'gym-tracker-v2026-01-10-01';
 const urlsToCache = [
   '/',
   '/gym-tracker.html',
